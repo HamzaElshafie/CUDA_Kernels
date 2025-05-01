cd469c4b0c5ab7e81bcbd127fcf9a92bbe0da133
HamzaElshafie <hamzaelshafie@outlook>
2025-05-01 22:20:25 +0300

    Completed initialiseArrays and cuda check in .h utility file.

--- utils.h
+++ utils.h
@@ -1,38 +1,47 @@
+/**
+ * @file utils.h
+ * @brief Utility functions for CUDA development
+ *
+ * This header provides core utility functions focusing on error checking timing,
+ * initialising arrays in memory and result comparison.
+ */
+
+#ifndef UTILS_H
+#define UTILS_H
+
 #include <iostream>
 #include <chrono>
 #include <cstdlib>
 #include <ctime>
 
-void initialiseVectors(float *A, float *B, int N)
-{
-    srand(static_cast<unsigned int>(time(0)));
+#define CUDA_CHECK(call) \
+do { \ 
+    cudaError_t error = call; \
+    if (error != cudaSuccess) { \
+        std::cerr << "CUDA error at " << __FILE__ << __LINE__ << " - " \
+        << cudaGetErrorString(error) << " (" << error << ") " << std::endl; \
+        exit(EXIT_FAILURE); \
+} \
+}while(0)
 
-    for (int i = 0; i < N; i++)
+void initialiseArrays(float** arrays, int num_arrays, size_t size, float min=0.0f, float max=1.0f, unsigned int seed=0)
+{
+    // Set random seed
+    if (seed == 0)
     {
-        A[i] = static_cast<float>(rand()); // divide by RAND_MAX later if you want to normalise values
-        B[i] = static_cast<float>(rand());
+        seed = static_cast<unsigned int>(time(0)); // get current time
     }
-}
+    srand(seed);
 
-template <typename Func>
-double measureExecutionTime(Func func)
-{
-    auto start = std::chrono::high_resolution_clock::now();
-    func();
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double, std::milli> duration = end - start;
-    return duration.count();
-}
+    float range = max - min;
 
-bool compareResults(const float *A, const float *B, int N)
-{
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < num_arrays; i++) // Iterate through each array pointer
     {
-        if (fabs(A[i] - B[i]) > 1e-4)
+        for (size_t j = 0; j < size; j++) // Iterate through each element
         {
-            std::cout << "Mismatch at index " << i << ": CPU=" << A[i] << " GPU=" << B[i] << std::endl;
-            return false;
+            arrays[i][j] = min + (static_cast<float>(rand()) / RAND_MAX) * range;
         }
     }
-    return true;
 }
+
+#endif
