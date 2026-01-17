24c8073da6caf8eb1dbf0619d62a36c58b54aced
HamzaElshafie <hamzaelshafie@outlook.com>
2026-01-17 18:24:55 +0200

    col to row  transpose logical.

---
+++ matrix_transpose/transpose_col2row.cu
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <cuda_fp16.h>
+#include <cuda_fp8.h>
+#include <cuda_runtime.h>
+#include <float.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <torch/extension.h>
+#include <torch/types.h>
+#include <vector>
+
+
+/**
+Tranposing kernel. Naive row-major tranposed (KxM) → row-major original (KxN).
+
+Steps:
+1. Find global index of thread to find its corresponding element
+1. Find the global row index of thread
+2. Find the global col index of thread
+*/ 
+__global__ void mat_transpose_f32(float* x, float* y, const int M, const int K) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = idx / K;
+    int col = idx % K;
+
+    if (idx < M * K) {
+        y[row * K + col] = x[col * M + row];
+    }
+}
